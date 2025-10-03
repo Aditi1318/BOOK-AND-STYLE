15b9c0396350a0da5a2d3b75b4f07200f51365f6
Aditi1318 <aditikushwaha.1318@gmail.com>
2025-10-03 19:28:23 +0530

    Added hover popup + glowing effect to SalonCard

--- frontend/src/Customer/pages/Salon/SalonList.jsx
+++ frontend/src/Customer/pages/Salon/SalonList.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import SalonCard from "./SalonCard";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchSalons } from "../../../Redux/Salon/action";
 
 const SalonList = ({salons}) => {
-
-  return (
-    <div className="flex gap-6 flex-wrap ">
-      {salons?.map((item) => (
-        <SalonCard key={item.id} salon={item}/>
-      ))}
-    </div>
-  );
+    return (
+        <div className="flex gap-6 flex-wrap">
+            {salons?.map((item) => (
+                <div
+                    key={item.id}
+                    className="transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_30px_rgba(144,238,144,0.8)] rounded-xl"
+                >
+                    <SalonCard salon={item} />
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default SalonList;
