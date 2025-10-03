fdc53c2e9dea10346640dfccd9f7b890a7581640
Aditi1318 <aditikushwaha.1318@gmail.com>
2025-10-03 20:03:53 +0530

    Added Footer to SalonDashboard

--- frontend/src/salon/pages/SellerDashboard/SalonDashboard.jsx
+++ frontend/src/salon/pages/SellerDashboard/SalonDashboard.jsx
@@ -1,35 +1,40 @@
-import React, { useEffect } from "react";
-
+import React, {useEffect} from "react";
 
 import Navbar from "../../../admin seller/components/navbar/Navbar";
 import SalonRoutes from "../../../routes/SalonRoutes";
-import { useDispatch } from "react-redux";
-import { fetchSalonByOwner } from "../../../Redux/Salon/action";
+import {useDispatch} from "react-redux";
+import {fetchSalonByOwner} from "../../../Redux/Salon/action";
 import SalonDrawerList from "../../components/SideBar/DrawerList";
-import { getSalonReport } from "../../../Redux/Booking/action";
+import {getSalonReport} from "../../../Redux/Booking/action";
+import Footer from "../../../Customer/pages/Footer/Footer";
 
 const SalonDashboard = () => {
-  const dispatch=useDispatch();
-  // const {}
-  useEffect(() => {
-    dispatch(fetchSalonByOwner(localStorage.getItem("jwt")));
-    dispatch(getSalonReport(localStorage.getItem("jwt")))
-  }, []);
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(fetchSalonByOwner(localStorage.getItem("jwt")));
+        dispatch(getSalonReport(localStorage.getItem("jwt")));
+    }, [dispatch]);
 
-  return (
-    <div className="min-h-screen">
-      <Navbar DrawerList={SalonDrawerList}/>
-      <section className="lg:flex lg:h-[90vh]">
-        <div className="hidden lg:block h-full">
-        <SalonDrawerList/>
+    return (
+        <div className="min-h-screen flex flex-col">
+            {/* Navbar */}
+            <Navbar DrawerList={SalonDrawerList} />
+
+            {/* Main Content */}
+            <section className="lg:flex lg:h-[90vh] flex-1">
+                <div className="hidden lg:block h-full">
+                    <SalonDrawerList />
+                </div>
+                <div className="p-10 w-full lg:w-[80%] overflow-y-auto">
+                    <SalonRoutes />
+                </div>
+            </section>
+
+            {/* Footer */}
+            <Footer />
         </div>
-        <div className="p-10 w-full lg:w-[80%]  overflow-y-auto">
-          <SalonRoutes />
-        </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default SalonDashboard;
